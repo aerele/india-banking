a27b0dbe52ff4b8e44582742627fdb148065ea55
Bhavan23 <bhavansathru.it@gmail.com>
2024-08-13 14:59:02 +0530

    fix: fixed Data error while create api log

--- india_banking/india_banking/doctype/india_banking_request_log/india_banking_request_log.py
+++ india_banking/india_banking/doctype/india_banking_request_log/india_banking_request_log.py
@@ -19,17 +19,20 @@ def create_api_log(res, action= None, ref_doctype= None, ref_docname= None):
 		request_from (str): It is optional for the purposes of the API...
 	"""
 	if not res: return
-	
-	log_doc = frappe.new_doc("India Banking Request Log")
-	log_doc.action = action
-	log_doc.url = res.request.url
-	log_doc.method = res.request.method
-	log_doc.header = json.dumps(dict(res.request.headers))
-	log_doc.payload =res.request.body
-	log_doc.response = json.dumps(res.json())
-	log_doc.status_code = res.status_code
-	log_doc.reference_doctype = ref_doctype
-	log_doc.reference_docname = ref_docname
-	log_doc.save()
-	
-	frappe.db.commit()
+
+	try:
+		log_doc = frappe.new_doc("India Banking Request Log")
+		log_doc.action = action
+		log_doc.url = res.request.url
+		log_doc.method = res.request.method
+		log_doc.header = json.dumps(dict(res.request.headers), indent=4)
+		log_doc.payload =json.dumps(res.request.body, indent=4)
+		log_doc.response = json.dumps(res.json(), indent=4)
+		log_doc.status_code = res.status_code
+		log_doc.reference_doctype = ref_doctype
+		log_doc.reference_docname = ref_docname
+		log_doc.save()
+	except:
+		frappe.log_error(title='Error in creating API Log', message=frappe.get_traceback())
+	else:
+		frappe.db.commit()

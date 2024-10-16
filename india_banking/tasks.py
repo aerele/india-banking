827e526856eac745abfa749696d7aea122f90618
Bhavan23 <bhavansathru.it@gmail.com>
2024-10-16 13:50:04 +0530

    fix: remove debuger code

--- india_banking/tasks.py
+++ india_banking/tasks.py
@@ -1,15 +1,12 @@
 import frappe
 from india_banking.india_banking.doc_events.payment_order import get_payment_status
 def daily():
-	frappe.log_error("Daily Cron Running")
 	orders = frappe.get_all("Payment Order Summary", 
 		{
 			'docstatus': 1, 
 			'payment_status': 'Initiated'
 		},
 		pluck= 'parent', distinct='parent')
-	
-	frappe.log_error("Orders : ", str(orders))
 
 	for order in orders:
 		try:

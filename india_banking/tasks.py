f0b8ec8450add6f8846953a0059ea784a03ba6b4
Bhavan23 <bhavansathru.it@gmail.com>
2024-12-19 13:15:26 +0530

    refactor: format indentation

--- india_banking/tasks.py
+++ india_banking/tasks.py
@@ -1,17 +1,21 @@
 import frappe
+
 from india_banking.india_banking.doc_events.payment_order import get_payment_status
+
+
 def daily():
-	orders = frappe.get_all("Payment Order Summary", 
-		{
-			'docstatus': 1, 
-			'payment_status': 'Initiated'
-		},
-		pluck= 'parent', distinct='parent')
+	orders = frappe.get_all(
+		"Payment Order Summary",
+		{"docstatus": 1, "payment_status": "Initiated"},
+		pluck="parent",
+		distinct="parent",
+	)
 
 	for order in orders:
 		try:
-			frappe.enqueue(
-				get_payment_status, docname= order, queue="short"
-			)
+			frappe.enqueue(get_payment_status, docname=order, queue="short")
 		except:
-			frappe.log_error(title="Error in Payment Order Status Cron", message=frappe.get_traceback())
+			frappe.log_error(
+				title="Error in Payment Order Status Cron",
+				message=frappe.get_traceback(),
+			)

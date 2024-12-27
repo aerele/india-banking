0cf81917a3438942b445cd5f19f92a38469f66eb
Bhavan23 <bhavansathru.it@gmail.com>
2024-12-27 16:58:55 +0530

    refactor: optimise bank doc_events code

--- india_banking/india_banking/doc_events/bank.py
+++ india_banking/india_banking/doc_events/bank.py
@@ -4,15 +4,16 @@ import frappe
 from frappe import _
 from frappe.utils import cstr
 
+IFSC_PATTERN = re.compile(r"^[A-Z]{4}0[A-Z0-9]{6}$")
+
 
 def bank_on_trash(doc, method=None):
-	if hasattr(doc, "is_standard") and doc.is_standard:
+	if getattr(doc, "is_standard", False):
 		frappe.throw(
 			_("Standard Bank cannot be deleted"), title=_("Action Not Permitted")
 		)
 
 
 def validate_ifsc_code(doc, method=None):
-	pattern = re.compile("^[A-Z]{4}0[A-Z0-9]{6}$")
-	if not pattern.match(cstr(doc.branch_code)):
+	if not IFSC_PATTERN.match(cstr(doc.branch_code)):
 		frappe.throw(_("IFSC/Branch Code is not valid"))

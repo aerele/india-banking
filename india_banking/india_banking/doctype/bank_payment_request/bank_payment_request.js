7a4f927a709aebbe53a5a83b30fca84d6489c0ab
Bhavan23 <bhavansathru.it@gmail.com>
2024-12-27 15:25:02 +0530

    refactor: recreate Bank Payment Request with disabled create options

---
+++ india_banking/india_banking/doctype/bank_payment_request/bank_payment_request.js
@@ -0,0 +1,107 @@
+// Copyright (c) 2024, Aerele Technologies Private Limited and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Bank Payment Request', {
+	setup: function (frm) {
+		frm.set_query("party_type", function () {
+			return {
+				filters: {
+                    "name": ["in", ["Supplier", "Employee"]]
+                }
+			};
+		});
+	},
+	refresh(frm) {
+		frm.set_query("payment_type", function() {
+			return {
+				filters: {
+					"company": frm.doc.company
+				}
+			};
+		});
+
+		frm.set_query("cost_center", function() {
+			return {
+				filters: {
+					"is_group": 0,
+					"disabled": 0
+				}
+			};
+		});
+
+		frm.set_query("mode_of_payment", function() {
+			return {
+				filters: {
+					"name": "Wire Transfer"
+				}
+			};
+		});
+
+		setTimeout(() => {
+			frm.trigger('toggle_custom_button')
+		}, 500);
+	},
+	toggle_custom_button(frm){
+		if(frm.doc.status == "Initiated") {
+			frm.remove_custom_button(__('Create Payment Entry'))
+		}
+	},
+
+	company (frm) {
+		frm.set_query("payment_type", function() {
+			return {
+				filters: {
+					"company": frm.doc.company
+				}
+			};
+		});
+	},
+	mode_of_payment (frm) {
+		var conditions = get_bank_query_conditions(frm);
+		if (frm.doc.mode_of_payment == "Wire Transfer") {
+			frm.set_query("bank_account", function() {
+				return {
+					filters: conditions
+				};
+			});
+		}
+	},
+	party_type (frm) {
+		var conditions = get_bank_query_conditions(frm);
+		if (frm.doc.mode_of_payment == "Wire Transfer") {
+			frm.set_query("bank_account", function() {
+				return {
+					filters: conditions
+				};
+			});
+		}
+	},
+	party (frm) {
+		var conditions = get_bank_query_conditions(frm);
+		if (frm.doc.mode_of_payment == "Wire Transfer") {
+			frm.set_query("bank_account", function() {
+				return {
+					filters: conditions
+				};
+			});
+		}
+	}
+});
+
+const get_bank_query_conditions = function(frm) {
+	var conditions = {}
+	if (frm.doc.party_type) {
+		conditions["party_type"] = frm.doc.party_type;
+	}
+	if (frm.doc.party) {
+		conditions["party"] = frm.doc.party;
+	}
+	if (frm.doc.mode_of_payment == "Wire Transfer") {
+		frm.set_query("bank_account", function() {
+			return {
+				filters: conditions
+			};
+		});
+	}
+	return conditions;
+};

2d84dc0d0ab12a2d9eb1a4f5ea14dbcf00f54169
Bhavan23 <bhavansathru.it@gmail.com>
2025-01-10 15:33:12 +0530

    refactor: remove custom field deletetion without checking filter.

--- india_banking/patches/v1_0/update_custom_fields.py
+++ india_banking/patches/v1_0/update_custom_fields.py
@@ -7,37 +7,4 @@ from india_banking.uninstall import delete_custom_fields
 def execute():
 	toggle_payment_request_creation()
 	delete_custom_fields()
-	remove_custom_section_and_column_break_fields()
-	remove_main_field_order()
 	make_custom_fields()
-
-
-def remove_custom_section_and_column_break_fields():
-	if fields := frappe.get_all(
-		"Custom Field",
-		filters={
-			"dt": [
-				"in",
-				[
-					"Journal Entry Account",
-					"Payment Entry",
-					"Payment Order",
-					"Payment Order Reference",
-					"Payment Request",
-				],
-			],
-			"fieldtype": ["in", ["Section Break", "Column Break"]],
-		},
-		pluck="name",
-	):
-		frappe.db.delete("Custom Field", {"name": ["in", fields]})
-
-
-def remove_main_field_order():
-	doctypes = [
-		"Payment Order",
-		"Payment Order Reference",
-		"Payment Request",
-	]
-	for doctype in doctypes:
-		frappe.db.delete("Property Setter", {"name": f"{doctype}-main-field_order"})

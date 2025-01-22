e614e96e22542ba743a4f1459de4bf0e735b3b3c
Bhavan23 <bhavansathru.it@gmail.com>
2025-01-22 17:17:59 +0530

    fix: add unrecocil option for ammend record

--- india_banking/india_banking/doc_events/unreconcile_payment.py
+++ india_banking/india_banking/doc_events/unreconcile_payment.py
@@ -1,19 +1,33 @@
 import frappe
 
 def on_submit(doc, method=None):
-    if doc.voucher_type == "Payment Entry":
-        if frappe.db.get_value("Payment Entry", doc.voucher_no, "source_doctype") != "Bank Payment Request":
-            return
+	if not doc.voucher_type == "Payment Entry":
+		return
 
-    payment_order_summary = frappe.get_doc("Payment Order Summary", {"payment_entry": doc.voucher_no})
-    payment_order = frappe.get_doc("Payment Order", payment_order_summary.parent)
+	if frappe.db.get_value("Payment Entry", doc.voucher_no, "source_doctype") != "Bank Payment Request":
+		return
 
-    for reference in payment_order.references:
-        filter_condition = ( payment_order_summary.party_type == reference.party_type and payment_order_summary.party == reference.party and payment_order_summary.cost_center == reference.cost_center
-                and payment_order_summary.project == reference.project and payment_order_summary.bank_account == reference.bank_account and payment_order_summary.account == reference.account
-                and payment_order_summary.tax_withholding_category == reference.tax_withholding_category and payment_order_summary.reference_doctype == reference.reference_doctype )
-        if not payment_order.is_party_wise or payment_order.get('summarise_payment_based_on') == "Voucher":
-            filter_condition = filter_condition and (payment_order_summary.reference_doctype == reference.reference_doctype and payment_order_summary.reference_name == reference.reference_name)
-        if filter_condition:
-            frappe.db.set_value("Bank Payment Request", reference.bank_payment_request, {"reference_doctype": "", "reference_name": ""})
-            frappe.db.set_value("Payment Order Reference", reference.name, {"reference_doctype": "", "reference_name": ""})
+	payment_order_summary = get_payment_order_summary(doc.voucher_no)
+
+	if not payment_order_summary:
+		return
+
+	payment_order = frappe.get_doc("Payment Order", payment_order_summary.parent)
+
+	for reference in payment_order.references:
+		filter_condition = ( payment_order_summary.party_type == reference.party_type and payment_order_summary.party == reference.party and payment_order_summary.cost_center == reference.cost_center
+				and payment_order_summary.project == reference.project and payment_order_summary.bank_account == reference.bank_account and payment_order_summary.account == reference.account
+				and payment_order_summary.tax_withholding_category == reference.tax_withholding_category and payment_order_summary.reference_doctype == reference.reference_doctype )
+		if not payment_order.is_party_wise or payment_order.get('summarise_payment_based_on') == "Voucher":
+			filter_condition = filter_condition and (payment_order_summary.reference_doctype == reference.reference_doctype and payment_order_summary.reference_name == reference.reference_name)
+		if filter_condition:
+			frappe.db.set_value("Bank Payment Request", reference.bank_payment_request, {"reference_doctype": "", "reference_name": ""})
+			frappe.db.set_value("Payment Order Reference", reference.name, {"reference_doctype": "", "reference_name": ""})
+
+def get_payment_order_summary(payment_entry):
+	is_ammended = frappe.db.get_value("Payment Entry", payment_entry, "amended_from")
+	payment_entry = "-".join(payment_entry.split("-")[:-1]) if is_ammended else payment_entry
+	summary = frappe.db.get_value("Payment Order Summary", {"payment_entry": payment_entry}, "name")
+
+	if summary:
+		return frappe.get_doc("Payment Order Summary", summary)

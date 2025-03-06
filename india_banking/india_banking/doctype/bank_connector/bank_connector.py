14a6bd7d444d753d7d7f6e55845f75418dfbfafb
Bhavan23 <bhavansathru.it@gmail.com>
2025-03-06 12:47:15 +0530

    feat: benificery api configuration added.

--- india_banking/india_banking/doctype/bank_connector/bank_connector.py
+++ india_banking/india_banking/doctype/bank_connector/bank_connector.py
@@ -1,9 +1,60 @@
 # Copyright (c) 2024, Aerele Technologies Private Limited and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+import requests
+import json
 from frappe.model.document import Document
+from india_banking.india_banking.doctype.india_banking_request_log.india_banking_request_log import (
+	create_api_log,
+)
 
 
 class BankConnector(Document):
-	pass
+	def post_request(self, bank_account_doc, action=None):
+		url = f"{self.url}/api/method/india_banking_connector.api.connect"
+		
+		payment_payload = frappe._dict({})
+
+		payment_payload.method = "update_benificery_details"
+		payment_payload.doc = bank_account_doc.as_dict()
+		payment_payload.doc.action = action
+
+		api_key = self.api_key
+		api_secret = self.get_password("api_secret")
+		headers = {
+			"Authorization": f"token {api_key}:{api_secret}",
+			"Content-Type": "application/json",
+		}
+		response = requests.request(
+			"POST", url, headers=headers, data=json.dumps(payment_payload)
+		)
+
+		# create api response log
+		create_api_log(
+			response, "Update Benificery Details", bank_account_doc.doctype, bank_account_doc.name
+		)
+
+		if response.ok:
+			response_details = response.json().get("message")
+			if association_id:= response_details.get("association_id"):
+				frappe.db.set_value("Bank Account", bank_account_doc.name, "association_id", association_id)
+			elif response_details.get("status") == "success":
+				frappe.msgprint(response_details.get("message"), alert=1, indicator="green")
+			else:
+				frappe.msgprint(response_details.get("message"), alert=1, indicator="red")
+		else:
+			frappe.throw("Invalid Request")
+
+
+@frappe.whitelist()
+def update_benificery_details(bank_account, action=None):
+	bank_account_doc = frappe.get_doc("Bank Account", bank_account)
+
+	bank_connector_exists = frappe.db.exists(
+		"Bank Connector", {"company": bank_account_doc.company, "bank": bank_account_doc.bank}
+	)
+
+	bank_connector = frappe.get_doc("Bank Connector", bank_connector_exists)
+
+	bank_connector.post_request(bank_account_doc, action)
--- india_banking/public/js/bank_account.js
+++ india_banking/public/js/bank_account.js
@@ -26,6 +26,24 @@ frappe.ui.form.on('Bank Account', {
 				}
 			};
 		});
+		frm.events.add_benificery_actions(frm)
+	},
+	add_benificery_actions(frm){
+		debugger
+		frm.add_custom_button("Add Benificery", frm.events.update_benificery_details(frm, "Create"), "Benificery Action")
+		frm.add_custom_button("Update Benificery", frm.events.update_benificery_details(frm, "Update"), "Benificery Action")
+		frm.add_custom_button("Discard Benificery", frm.events.update_benificery_details(frm, "Discard"), "Benificery Action")
+		frm.add_custom_button("Approve Benificery", frm.events.update_benificery_details(frm, "Approve"), "Benificery Action")
+		frm.add_custom_button("Reject Benificery", frm.events.update_benificery_details(frm, "Reject"), "Benificery Action")
+		frm.add_custom_button("Suspend Benificery", frm.events.update_benificery_details(frm, "Suspend"), "Benificery Action")
+	},
+	update_benificery_details(frm, action){
+		frm.call({
+			method: "update_benificery_details",
+			action: action,
+			freeze: 1,
+			freeze_message: "Updating..."
+		})
 	},
 	onload(frm){
 		if (frm.doc.workflow_state == 'Approved') {

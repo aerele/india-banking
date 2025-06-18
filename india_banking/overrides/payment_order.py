7cc9a41bceb3472d8400d36701e4f44b0e26bed8
Bhavan23 <bhavansathru.it@gmail.com>
2025-06-18 12:49:47 +0530

    fix(payment-order): add validation for intra-bank mode of transfer

--- india_banking/overrides/payment_order.py
+++ india_banking/overrides/payment_order.py
@@ -82,6 +82,12 @@ class CustomPaymentOrder(PaymentOrder):
 						f"Invalid mode of transfer for {payment.party_type} - {payment.party} at <b>row #{payment.idx}</b>"
 					)
 				)
+			if payment.bank == self.company_bank and "A2A" not in mode_of_transfer.mode:
+				frappe.throw(
+					_(
+						f"Invalid mode of transfer for {payment.party_type} - {payment.party} at <b>row #{payment.idx}</b>. A2A is required for intra-bank transfers."
+					)
+				)
 
 			if not mode_of_transfer:
 				frappe.throw(_("Define a specific mode of transfer or a default one"))

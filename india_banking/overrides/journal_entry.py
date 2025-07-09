c0884f0dfb17655e1ca702b4aee28fb104f7fc67
Bhavan23 <bhavansathru.it@gmail.com>
2025-07-09 23:01:37 +0530

    fix: correct bank entry fetching logic.

--- india_banking/overrides/journal_entry.py
+++ india_banking/overrides/journal_entry.py
@@ -4,122 +4,153 @@ from erpnext.accounts.doctype.accounting_dimension.accounting_dimension import (
 )
 from frappe import _
 from frappe.query_builder import DocType
-from frappe.query_builder.functions import Sum
-from frappe.utils import get_url_to_form
+from frappe.utils import get_link_to_form
+from pypika.terms import ExistsCriterion
 
 
 @frappe.whitelist()
 def make_payment_order(source_name, target_doc=None, args=None):
 	from frappe.model.mapper import get_mapped_doc
 
-	def validate_party_bank_account(bank_entry=None):
-		if not bank_entry:
-			return
+	def validate_party_bank_account(
+		party_details, party_bank_details, invalid_party_details
+	):
+		for party_detail in party_details:
+			party_type, party = party_detail.values()
+			msg = ""
+			if (party_type, party) in party_bank_details:
+				continue
 
-		JournalEntryAccount = DocType("Journal Entry Account")
-		BankAccount = DocType("Bank Account")
-
-		bank_account_query = (
-			frappe.qb.from_(JournalEntryAccount)
-			.left_join(BankAccount)
-			.on(JournalEntryAccount.party == BankAccount.party)
-			.select(
-				JournalEntryAccount.party_type,
-				JournalEntryAccount.party,
-				BankAccount.name.as_("bank_account"),
+			bank_account = frappe.get_value(
+				"Bank Account",
+				{
+					"party_type": party_type,
+					"party": party,
+				},
+				["name", "disabled", "is_default"],
+				as_dict=1,
 			)
-			.where(
-				(JournalEntryAccount.parent == bank_entry)
-				& (
-					JournalEntryAccount.payment_status.notin(
-						["Paid", "Ordered", "Payment Ordered"]
-					)
-				)
-				& (
-					(BankAccount.name.isnull())
-					| (BankAccount.disabled == 1)
-					| (BankAccount.is_default == 0)
-				)
-			)
-			.groupby(JournalEntryAccount.party)
-		)
 
-		non_bank_account_party = bank_account_query.run(as_dict=1)
+			if not bank_account:
+				msg += f"<b>{party_type}-{party}</b> does not have a bank account.<br>"
+			if bank_account and not bank_account.is_default:
+				msg += f"<b>{party_type}-{party}</b> has no default bank account.<br>"
+			if bank_account and bank_account.disabled:
+				bank_account_link = get_link_to_form("Bank Account", bank_account.name)
+				msg += f"<b>{party_type}-{party}</b> bank account {bank_account_link} is disabled.<br>"
 
-		msg = ""
-		for party_details in non_bank_account_party:
-			if (
-				party_details
-				and (party_type := party_details.get("party_type"))
-				and (party := party_details.get("party"))
-			):
-				if not party_details.get("bank_account"):
-					msg += (
-						f"<b>{party_type}-{party}</b> does not have a bank account.<br>"
-					)
-				else:
-					msg += (
-						f"<b>{party_type}-{party}</b> has no default bank account.<br>"
-					)
-		if msg:
-			frappe.msgprint(
-				_(
-					"We can see Some bank entries are missing bank account details and have been ignored. Please update the bank account information and try again.</br></br><p style='color:red'><b>Missing Details are below</b></p>"
-					+ msg
-				),
-				title=_("Missing Bank Account"),
-				indicator="orange",
-			)
+			if msg:
+				if msg not in invalid_party_details:
+					invalid_party_details.append(msg)
+			else:
+				party_bank_details.update({(party_type, party): bank_account.name})
 
 	def update_bank_entry(source, target):
-		JournalEntryAccount = DocType("Journal Entry Account")
-		BankAccount = DocType("Bank Account")
+		net_payable = 0
+		net_receivable = 0
 
-		select_field = [
-			"name",
-			"account",
-			"cost_center",
-			"project",
-			"debit as amount",
-			"party",
-			"party_type",
-			"parent as journal",
-		]
-		select_field.extend(get_accounting_dimensions())
+		party_receivables = {}
+		invalid_party_details = []
+		party_bank_details = {}
 
-		# Validate party does NOT have a valid Bank Account
-		validate_party_bank_account(source.name)
-
-		# Build the query
-		query = (
-			frappe.qb.from_(JournalEntryAccount)
-			.join(BankAccount)
-			.on(JournalEntryAccount.party == BankAccount.party)
-			.select(
-				*[
-					getattr(JournalEntryAccount, field.split(" as ")[0]).as_(
-						field.split(" as ")[1]
-					)
-					if " as " in field
-					else getattr(JournalEntryAccount, field)
-					for field in select_field
-				],
-				BankAccount.name.as_("party_bank_account"),
+		if source.accounts:
+			party_details = [
+				{"party_type": acc.party_type, "party": acc.party}
+				for acc in source.accounts
+				if acc.party_type and acc.party
+			]
+			validate_party_bank_account(
+				party_details, party_bank_details, invalid_party_details
 			)
-			.where(
-				(JournalEntryAccount.parent == source.name)
-				& (
-					JournalEntryAccount.payment_status.notin(
-						["Paid", "Ordered", "Payment Ordered"]
+			if invalid_party_details:
+				if msg := "".join(invalid_party_details):
+					journal_entry_link = get_link_to_form("Journal Entry", source.name)
+					frappe.msgprint(
+						_(
+							(
+								"We can see Some bank entries are missing bank account details and have been ignored."
+								"Please update the bank account information and try again."
+								f"</br></br><p style='color:red'><b>The missing details for {journal_entry_link} are provided below.</b></p>"
+							)
+							+ msg
+						),
+						title=_("Missing Bank Account"),
+						indicator="orange",
+					)
+					return
+
+			for acc in source.accounts:
+				if acc.account == target.account:
+					net_payable += (
+						acc.debit_in_account_currency - acc.credit_in_account_currency
+					)
+				else:
+					if acc.party_type and acc.party:
+						key = (acc.party_type, acc.party)
+						if key in party_receivables:
+							net_receivable += (
+								acc.debit_in_account_currency
+								- acc.credit_in_account_currency
+							)
+							receivables = (
+								acc.debit_in_account_currency
+								- acc.credit_in_account_currency
+							)
+							net_receivable += receivables
+							party_receivables[key].payable_amount += receivables
+						else:
+							party_receivables[key] = acc
+							receivables = (
+								acc.debit_in_account_currency
+								- acc.credit_in_account_currency
+							)
+							net_receivable += receivables
+							party_receivables[key].payable_amount = receivables
+
+			amount = net_payable + net_receivable
+			if amount > 0:
+				entry_link = get_link_to_form("Journal Entry", source.name)
+				frappe.msgprint(
+					_(
+						f"Bank Entry {frappe.bold(entry_link)} is ambiguous and will be ignored."
 					)
 				)
-				& (BankAccount.disabled == 0)
-				& (BankAccount.is_default == 1)
-			)
-			.groupby(JournalEntryAccount.name)
+				return
+
+		ordered_bank_entries = frappe.get_all(
+			"Payment Order Reference",
+			filters={
+				"docstatus": ["in", [0, 1]],
+				"reference_doctype": "Journal Entry",
+				"parent": ["!=", target.name],
+			},
+			fields=["reference_doctype", "reference_name", "journal_entry_account"],
+			order_by="idx",
+			as_list=True,
 		)
 
-		journal_accounts = query.run(as_dict=True)
+		already_fetched = [
+			(
+				reference.reference_doctype,
+				reference.reference_name,
+				reference.journal_entry_account,
+			)
+			for reference in target.references
+		]
+		journal_accounts = []
+
+		for entry in party_receivables.values():
+			if entry.payable_amount > 0:
+				if (entry.parenttype, entry.parent, entry.name) in ordered_bank_entries:
+					continue
+				if (entry.parenttype, entry.parent, entry.name) in already_fetched:
+					continue
+				entry.payment_amount = entry.payable_amount
+				entry.party_bank_account = party_bank_details[
+					(entry.party_type, entry.party)
+				]
+				if entry.party_bank_account:
+					journal_accounts.append(entry)
 
 		target.payment_order_type = "Journal Entry"
 		target.docstaus = 0
@@ -132,48 +163,11 @@ def make_payment_order(source_name, target_doc=None, args=None):
 			}
 
 		for journal_account in journal_accounts:
-			bank_account = frappe.get_doc(
-				"Bank Account", journal_account.party_bank_account
-			)
-			if frappe.db.get_single_value(
-				"India Banking Settings", "activate_workflow_on_bank_account"
-			):
-				if bank_account.workflow_state != "Approved":
-					frappe.throw(
-						title=_("Cannot proceed with un-approved bank account"),
-						msg=_(
-							"{}-{}- Bank Account <a href='{}'>{}</a>".format(
-								journal_account.party_type,
-								journal_account.party,
-								get_url_to_form(
-									"Bank Account", journal_account.party_bank_account
-								),
-								frappe.bold(journal_account.party_bank_account),
-							)
-						),
-					)
-
-			if bank_account.currency != "INR":
-				frappe.throw(
-					title=_("The party bank account currency should be in INR."),
-					msg=_(
-						"{}-{}- Bank Account <a href='{}'>{}</a>".format(
-							journal_account.party_type,
-							journal_account.party,
-							get_url_to_form(
-								"Bank Account", journal_account.party_bank_account
-							),
-							frappe.bold(journal_account.party_bank_account),
-						)
-					),
-				)
-
-			journal_account = frappe._dict(journal_account)
 			details = {
 				"reference_doctype": "Journal Entry",
-				"reference_name": journal_account.journal,
+				"reference_name": journal_account.parent,
 				"journal_entry_account": journal_account.name,
-				"amount": journal_account.amount,
+				"amount": journal_account.payment_amount,
 				"party_type": journal_account.party_type,
 				"party": journal_account.party,
 				"mode_of_payment": "",
@@ -205,42 +199,64 @@ def make_payment_order(source_name, target_doc=None, args=None):
 @frappe.validate_and_sanitize_search_inputs
 def get_bank_entry(doctype, txt, searchfield, start, page_len, filters, as_dict):
 	filters = frappe._dict(filters)
+
 	JournalEntry = DocType("Journal Entry")
 	JournalEntryAccount = DocType("Journal Entry Account")
+
+	ordered_bank_entries = frappe.get_all(
+		"Payment Order Reference",
+		filters={
+			"docstatus": ["in", [0, 1]],
+			"reference_doctype": "Journal Entry",
+		},
+		pluck="reference_name",
+	)
 
 	query = (
 		frappe.qb.from_(JournalEntry)
 		.join(JournalEntryAccount)
 		.on(JournalEntry.name == JournalEntryAccount.parent)
-		.where(
-			(JournalEntry.docstatus == 1)
-			& (
-				JournalEntryAccount.payment_status.notin(
-					["Paid", "Ordered", "Payment Ordered"]
-				)
-			)
-			& (JournalEntry.voucher_type == "Bank Entry")
-			& (JournalEntryAccount.against_account == filters.company_account)
-		)
-		.groupby(JournalEntry.name, JournalEntry.company, JournalEntry.voucher_type)
 		.select(
-			JournalEntry.name,
+			JournalEntryAccount.parent.as_("name"),
 			JournalEntry.company,
-			Sum(JournalEntryAccount.debit).as_("total"),
 			JournalEntry.voucher_type,
 		)
+		.where(
+			(JournalEntry.name.notin(ordered_bank_entries))
+			& (JournalEntry.docstatus == 1)
+			& (JournalEntry.voucher_type.eq("Bank Entry"))
+			& (
+				ExistsCriterion(
+					frappe.qb.from_(JournalEntryAccount)
+					.select("name")
+					.where(
+						(JournalEntryAccount.parent == JournalEntry.name)
+						& (JournalEntryAccount.account == filters.company_account)
+					)
+				)
+			)
+			& (
+				ExistsCriterion(
+					frappe.qb.from_(JournalEntryAccount)
+					.select("name")
+					.where(
+						(JournalEntryAccount.parent == JournalEntry.name)
+						& (JournalEntryAccount.account != filters.company_account)
+						& (JournalEntryAccount.party_type.isnotnull())
+						& (
+							JournalEntryAccount.payment_status.notin(
+								["Ordered", "Payment Ordered", "Paid"]
+							)
+						)
+					)
+				)
+			)
+		)
+		.groupby(JournalEntryAccount.parent)
 	)
 
-	if filters:
-		if filters.docs:
-			existing_entries = tuple(filters.docs or [])
-			query = query.where(JournalEntry.name.notin(existing_entries))
-		if filters.company:
-			query = query.where(JournalEntry.company == filters.company)
+	if searchfield:
+		if searchfield == "name":
+			query = query.where(JournalEntry.name.like(f"%{txt}%"))
 
-	if txt:
-		query = query.where(JournalEntry.name.like(f"%{txt}%"))
-
-	bank_entries = query.run(as_dict=as_dict)
-
-	return bank_entries
+	return query.run(as_dict=as_dict)

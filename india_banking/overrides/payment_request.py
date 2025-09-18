846cd96f78133def6fe4af2a235b2c88e922b80c
dharanidharan2813 <dharanidharans1328@gmail.com>
2025-09-18 18:47:38 +0530

    fix(payment-request): handle hold supplier gst payable from purchase order
    (cherry picked from commit 07acb0f35971610e0a1b8f18f4690ea54617c3c0)

--- india_banking/overrides/payment_request.py
+++ india_banking/overrides/payment_request.py
@@ -63,13 +63,17 @@ class BankPaymentRequest(PaymentRequest):
 			self.hold_gst_payables = frappe.db.get_value(
 				self.party_type, self.party, "hold_gst_payables"
 			)
+			if not self.hold_gst_payables:
+				return
 
 		ref_doc = frappe.get_doc(self.reference_doctype, self.reference_name)
 		existing_payment_request_amount = get_existing_payment_request_amount(ref_doc)
 		gst_payable_amount = self.get_gst_payable_amount()
 
 		if update:
-			applicable_payment_amount = self.grand_total - gst_payable_amount
+			applicable_payment_amount = (
+				self.grand_total - existing_payment_request_amount - gst_payable_amount
+			)
 			if applicable_payment_amount <= 0:
 				applicable_payment_amount = 0
 				link = get_link_to_report("Bank GST Payables")
@@ -77,9 +81,18 @@ class BankPaymentRequest(PaymentRequest):
 				msg += "<ul><li>Total Requested Amount: {}</li>".format(
 					bold(existing_payment_request_amount)
 				)
-				msg += "<li>Outstanding Amount: {}</li>".format(
-					bold(ref_doc.outstanding_amount)
-				)
+				if ref_doc.doctype == "Purchase Invoice":
+					msg += "<li>Outstanding Amount: {}</li>".format(
+						ref_doc.outstanding_amount
+					)
+				elif ref_doc.doctype == "Purchase Order":
+					msg += "<li>Order Total: {}</li>".format(
+						bold(ref_doc.base_grand_total)
+					)
+					msg += "<li>Advance Paid: {}</li>".format(
+						bold(ref_doc.advance_paid)
+					)
+
 				msg += "<li>Net Payable(GST Exclusive): {}</li>".format(
 					bold(applicable_payment_amount)
 				)
@@ -93,8 +106,15 @@ class BankPaymentRequest(PaymentRequest):
 			self.net_total = applicable_payment_amount
 		else:
 			if self.hold_gst_payables:
+				outstanding_amount = 0
+				if ref_doc.doctype == "Purchase Invoice":
+					outstanding_amount = ref_doc.outstanding_amount
+				elif ref_doc.doctype == "Purchase Order":
+					outstanding_amount = ref_doc.base_grand_total - flt(
+						ref_doc.advance_paid
+					)
 				applicable_payment_amount = (
-					ref_doc.outstanding_amount
+					outstanding_amount
 					- existing_payment_request_amount
 					- gst_payable_amount
 				)
@@ -108,7 +128,7 @@ class BankPaymentRequest(PaymentRequest):
 						),
 					)
 
-				if self.net_total > applicable_payment_amount:
+				if (self.net_total or self.grand_total) > applicable_payment_amount:
 					link = get_link_to_form("Supplier", self.party)
 					frappe.throw(
 						title="Hold Supplier GST Payable",
@@ -121,11 +141,11 @@ class BankPaymentRequest(PaymentRequest):
 		total_gst_payable_amount = 0
 		if (
 			self.reference_doctype
-			and self.reference_doctype == "Purchase Invoice"
+			and self.reference_doctype in ["Purchase Invoice", "Purchase Order"]
 			and self.reference_name
 		):
 			gst_payable_totals = frappe.db.get_all(
-				"Purchase Invoice Item",
+				f"{self.reference_doctype} Item",
 				filters={"parent": self.reference_name},
 				fields=[
 					"SUM(igst_amount) as total_igst",
@@ -136,9 +156,9 @@ class BankPaymentRequest(PaymentRequest):
 
 			total_igst, total_cgst, total_sgst = 0, 0, 0
 			if gst_payable_totals:
-				total_igst = gst_payable_totals[0].total_igst
-				total_cgst = gst_payable_totals[0].total_cgst
-				total_sgst = gst_payable_totals[0].total_sgst
+				total_igst = gst_payable_totals[0].total_igst or 0
+				total_cgst = gst_payable_totals[0].total_cgst or 0
+				total_sgst = gst_payable_totals[0].total_sgst or 0
 
 			total_gst_payable_amount = total_igst + total_cgst + total_sgst
 

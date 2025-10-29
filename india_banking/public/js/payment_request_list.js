e91e82474a46e0d7b2a2573ea13f4caba2656de1
Bhavan23 <bhavan@aerele.in>
2025-10-29 08:08:00 +0000

    fix: add adhoc request button in list view.

---
+++ india_banking/public/js/payment_request_list.js
@@ -0,0 +1,12 @@
+frappe.listview_settings["Payment Request"] = {
+	onload: function (listview) {
+		listview.page.add_inner_button(__("Make Adhoc Payment Request"), function () {
+			frappe.new_doc("Payment Request", {
+				payment_request_type: "Outward",
+				transaction_date: frappe.datetime.get_today(),
+				mode_of_payment: "Wire Transfer",
+				is_adhoc: 1,
+			});
+		});
+	},
+};

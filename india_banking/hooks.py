7e0d2faf122dd50e12460efdb5e8eae6ca334ad7
Bhavan23 <bhavan@aerele.in>
2025-12-04 06:18:40 +0000

    refactor(hooks): reorder hook mappings for logical grouping and readability

--- india_banking/hooks.py
+++ india_banking/hooks.py
@@ -6,56 +6,62 @@ app_email = "support@aerele.in"
 app_license = "gpl-3.0"
 
 
+before_uninstall = "india_banking.uninstall.before_uninstall"
 after_install = "india_banking.install.after_install"
 
-before_uninstall = "india_banking.uninstall.before_uninstall"
 
 before_tests = "india_banking.setup.utils.before_tests"
 
+
 doctype_js = {
-	"Payment Order": "public/js/payment_order.js",
-	"Purchase Invoice": "public/js/purchase_invoice.js",
 	"Bank Account": "public/js/bank_account.js",
+	"Purchase Invoice": "public/js/purchase_invoice.js",
 	"Payment Request": "public/js/payment_request.js",
+	"Payment Order": "public/js/payment_order.js",
 }
+
 
 doctype_list_js = {
-	"Payment Order": "public/js/payment_order_list.js",
-	"Purchase Invoice": "public/js/purchase_invoice_list.js",
 	"Payment Request": "public/js/payment_request_list.js",
+	"Purchase Invoice": "public/js/purchase_invoice_list.js",
+	"Payment Order": "public/js/payment_order_list.js",
 }
 
+
 override_doctype_class = {
-	"Payment Order": "india_banking.overrides.payment_order.CustomPaymentOrder",
 	"Payment Request": "india_banking.overrides.payment_request.BankPaymentRequest",
+	"Payment Order": "india_banking.overrides.payment_order.CustomPaymentOrder",
 }
+
 
 doc_events = {
 	"Bank": {
-		"on_trash": "india_banking.india_banking.doc_events.bank.bank.disallow_standard_bank_deletion"
+		"on_trash": "india_banking.india_banking.doc_events.bank.bank.disallow_standard_bank_deletion",
 	},
 	"Bank Account": {
-		"validate": "india_banking.india_banking.doc_events.bank_account.bank_account.validate"
-	},
-	"Unreconcile Payment": {
-		"on_submit": "india_banking.india_banking.doc_events.unreconcile_payment.on_submit",
-	},
-	"Payment Entry": {
-		"on_cancel": "india_banking.india_banking.doc_events.payment_entry.on_cancel",
-	},
-	"Payment Order": {
-		"autoname": "india_banking.utils.update_series",
+		"validate": "india_banking.india_banking.doc_events.bank_account.bank_account.validate",
 	},
 	"Payment Request": {
 		"autoname": "india_banking.utils.update_series",
 	},
+	"Payment Order": {
+		"autoname": "india_banking.utils.update_series",
+	},
+	"Payment Entry": {
+		"on_cancel": "india_banking.india_banking.doc_events.payment_entry.on_cancel",
+	},
+	"Unreconcile Payment": {
+		"on_submit": "india_banking.india_banking.doc_events.unreconcile_payment.on_submit",
+	},
 }
+
 
 accounting_dimension_doctypes = [
 	"Payment Order",
 	"Payment Order Reference",
 	"Payment Order Summary",
 ]
+
 
 scheduler_events = {
 	"daily": ["india_banking.tasks.daily"],

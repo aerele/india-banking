60e3fb898539651456203db7efddc48f4d09b6fb
Bhavan23 <bhavan@aerele.in>
2025-12-09 04:50:51 +0000

    test(payment_request): ad-hoc and reference validation

--- india_banking/overrides/payment_request/test_payment_request.py
+++ india_banking/overrides/payment_request/test_payment_request.py
@@ -27,19 +27,32 @@ class TestPaymentRequestOverrides(FrappeTestCase):
 		super().tearDown()
 		frappe.db.rollback()
 
+	def create_bank_payment_request(
+		self, is_adhoc, party_type, party, payment_request_type, net_total, **kwargs
+	):
+		return frappe.get_doc(
+			{
+				"is_adhoc": is_adhoc,
+				"doctype": "Payment Request",
+				"company": self.company.name,
+				"party_type": party_type,
+				"party": party,
+				"payment_request_type": payment_request_type,
+				"net_total": net_total,
+				"reference_doctype": kwargs.get("reference_doctype"),
+				"reference_name": kwargs.get("reference_name"),
+			}
+		)
+
 	def test_default_value_in_payment_request(self):
 		"""Test to verify default value in Payment Request document"""
 		# Create a new Payment Request document
-		payment_request = frappe.get_doc(
-			{
-				"is_adhoc": 1,
-				"doctype": "Payment Request",
-				"company": self.company.name,
-				"party_type": "Supplier",
-				"party": self.supplier.name,
-				"payment_request_type": "Outward",
-				"net_total": 1000,
-			}
+		payment_request = self.create_bank_payment_request(
+			is_adhoc=True,
+			party_type="Supplier",
+			party=self.supplier.name,
+			payment_request_type="Outward",
+			net_total=1500.00,
 		)
 		# Check default values before inserting the document
 		self.assertFalse(payment_request.grand_total)
@@ -54,3 +67,34 @@ class TestPaymentRequestOverrides(FrappeTestCase):
 		self.assertEqual(payment_request.transaction_date, frappe.utils.getdate())
 		self.assertEqual(payment_request.bank_account, self.supplier_bank_account.name)
 		self.assertEqual(payment_request.mode_of_payment, "Wire Transfer")
+
+	def test_ad_hoc_payment_with_reference_not_allowed(self):
+		"""Test to verify that ad-hoc payment with reference is not allowed"""
+		# Create a new Payment Request document with reference
+		payment_request = self.create_bank_payment_request(
+			is_adhoc=True,
+			party_type="Supplier",
+			party=self.supplier.name,
+			payment_request_type="Bank Payment",
+			net_total=2000.00,
+			reference_doctype="Purchase Order",
+		)
+
+		# Attempt to insert the document and expect a ValidationError
+		with self.assertRaises(frappe.ValidationError):
+			payment_request.insert()
+
+	def test_non_ad_hoc_payment_without_reference_not_allowed(self):
+		"""Test to verify that non ad-hoc payment without reference is not allowed"""
+		# Create a new Payment Request document without reference
+		payment_request = self.create_bank_payment_request(
+			is_adhoc=False,
+			party_type="Supplier",
+			party=self.supplier.name,
+			payment_request_type="Bank Payment",
+			net_total=2500.00,
+		)
+
+		# Attempt to insert the document and expect a ValidationError
+		with self.assertRaises(frappe.ValidationError):
+			payment_request.insert()

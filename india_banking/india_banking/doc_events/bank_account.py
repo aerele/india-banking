89061c6c3664871953eb0d47c2f80e5fd086dca4
Arnesh <arnesh1419@gmail.com>
2026-07-13 19:17:44 +0530

    fix: block duplicate receivers on Bank Account

--- india_banking/india_banking/doc_events/bank_account.py
+++ india_banking/india_banking/doc_events/bank_account.py
@@ -5,3 +5,20 @@ def validate_ifsc_code(self, method):
 	pattern = re.compile("^[A-Z]{4}0[A-Z0-9]{6}$")
 	if not pattern.match(cstr(self.branch_code)):
 		frappe.throw(_("IFSC/Branch Code is not valid"))
+
+
+def validate_duplicate_receivers(self, method):
+	first_row_idx = {}
+	for row in (self.get("beneficiaries") or []):
+		if not row.company or not row.receiver_id:
+			continue
+		key = (row.company, row.receiver_id)
+		if key in first_row_idx:
+			receiver = row.beneficiary_name or row.beneficiary or row.receiver_id
+			frappe.throw(
+				_("Row #{0}: Receiver {1} is already added for Company {2} (row #{3}).").format(
+					row.idx, frappe.bold(receiver), frappe.bold(row.company), first_row_idx[key]
+				),
+				title=_("Duplicate Receiver"),
+			)
+		first_row_idx[key] = row.idx
